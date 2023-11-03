91d25a8572de923e4d01499c1decd3604e4aac21
Alexandre Lamure <alexandre.lamure@epita.fr>
2023-11-03 01:38:24 +0100

    Add camera translation and rotation from inputs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { GUI } from 'dat.gui';
-import { mat4, vec3 } from 'gl-matrix';
+import { mat4, vec3, quat } from 'gl-matrix';
 import { Camera } from './camera';
 import { TriangleGeometry } from './geometries/triangle';
 import { GLContext } from './gl';
@@ -32,6 +32,9 @@ class Application {
 
   private _camera: Camera;
 
+  private _mouseClicked: boolean;
+  private _mouseCurrentPosition: { x: number, y: number };
+
   /**
    * Object updated with the properties from the GUI
    *
@@ -43,6 +46,9 @@ class Application {
     this._context = new GLContext(canvas);
     this._camera = new Camera();
     vec3.set(this._camera.position, 0.0, 0.0, 2.0);
+
+    this._mouseClicked = false;
+    this._mouseCurrentPosition = { x: 0, y: 0 };
 
     this._geometry = new TriangleGeometry();
     this._uniforms = {
@@ -76,6 +82,13 @@ class Application {
       // You can then use it directly as a uniform:
       // ```uniforms.myTexture = this._textureExample;```
     }
+
+    // Event handlers (mouse and keyboard)
+    canvas.addEventListener('keydown', this.onKeyDown, true);
+    canvas.addEventListener('pointerdown', this.onPointerDown, true);
+    canvas.addEventListener('pointermove', this.onPointerMove, true);
+    canvas.addEventListener('pointerup', this.onPointerUp, true);
+    canvas.addEventListener('pointerleave', this.onPointerUp, true);
   }
 
   /**
@@ -138,6 +151,58 @@ class Application {
     gui.addColor(this._guiProperties, 'albedo');
     return gui;
   }
+
+  /**
+   * Handle keyboard and mouse inputs to translate and rotate camera.
+   */
+  onKeyDown(event: KeyboardEvent) {
+    const speed = 0.2;
+
+    let forwardVec = vec3.fromValues(0.0, 0.0, -speed);
+    vec3.transformQuat(forwardVec, forwardVec, app._camera.rotation);
+    let rightVec = vec3.fromValues(speed, 0.0, 0.0);
+    vec3.transformQuat(rightVec, rightVec, app._camera.rotation);
+
+    if (event.key == 'z' || event.key == 'ArrowUp') {
+      vec3.add(app._camera.position, app._camera.position, forwardVec);
+    }
+    else if (event.key == 's' || event.key == 'ArrowDown') {
+      vec3.add(app._camera.position, app._camera.position, vec3.negate(forwardVec, forwardVec));
+    }
+    else if (event.key == 'd' || event.key == 'ArrowRight') {
+      vec3.add(app._camera.position, app._camera.position, rightVec);
+    }
+    else if (event.key == 'q' || event.key == 'ArrowLeft') {
+      vec3.add(app._camera.position, app._camera.position, vec3.negate(rightVec, rightVec));
+    }
+  }
+
+  onPointerDown(event: MouseEvent) {
+    app._mouseCurrentPosition.x = event.clientX;
+    app._mouseCurrentPosition.y = event.clientY;
+    app._mouseClicked = true;
+  }
+
+  onPointerMove(event: MouseEvent) {
+    if (!app._mouseClicked) {
+      return;
+    }
+
+    const dx = event.clientX - app._mouseCurrentPosition.x;
+    const dy = event.clientY - app._mouseCurrentPosition.y;
+    const angleX = dy * 0.002;
+    const angleY = dx * 0.002;
+    quat.rotateX(app._camera.rotation, app._camera.rotation, angleX);
+    quat.rotateY(app._camera.rotation, app._camera.rotation, angleY);
+
+    app._mouseCurrentPosition.x = event.clientX;
+    app._mouseCurrentPosition.y = event.clientY;
+  }
+
+  onPointerUp(event: MouseEvent) {
+    app._mouseClicked = false;
+  }
+
 }
 
 const canvas = document.getElementById('main-canvas') as HTMLCanvasElement;

565151c9b8e287b57d0dc6a8ece4b0b2027bc4f4
Alexandre Lamure <alexandre.lamure@epita.fr>
2023-11-16 17:24:21 +0100

    Add cartesianToPolar given code

--- src/shader/pbr.frag.ts
+++ src/shader/pbr.frag.ts
@@ -9,6 +9,15 @@ struct Material
 };
 uniform Material uMaterial;
 
+// Convert a unit cartesian vector to polar coordinates
+vec2 cartesianToPolar(vec3 cartesian) {
+    // Compute azimuthal angle, in [-PI, PI]
+    float phi = atan(cartesian.z, cartesian.x);
+    // Compute polar angle, in [-PI/2, PI/2]
+    float theta = asin(cartesian.y);
+    return vec2(phi, theta);
+}
+
 // From three.js
 vec4 sRGBToLinear( in vec4 value ) {
 	return vec4( mix( pow( value.rgb * 0.9478672986 + vec3( 0.0521327014 ), vec3( 2.4 ) ), value.rgb * 0.0773993808, vec3( lessThanEqual( value.rgb, vec3( 0.04045 ) ) ) ), value.a );
